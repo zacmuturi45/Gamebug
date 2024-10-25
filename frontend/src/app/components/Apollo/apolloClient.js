3d598e7a21c25ae588cff2ea711ba1aac9e5efbd
isaacgichuhi <zacmuturi45@gmail.com>
2024-10-25 18:20:36 +0300

    Changed ngrok url

--- frontend/src/app/components/Apollo/apolloClient.js
+++ frontend/src/app/components/Apollo/apolloClient.js
@@ -48,7 +48,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 
 //The HttpLink is responsible for sending the GraphQL requests to the specified server
 //uri: The URI is where the GraphQL server is hosted. In our case it points to a GraphQL endpoint hosted via an ngrok tunnel
-const httpLink = new HttpLink({ uri: "https://ad1f-102-217-67-238.ngrok-free.app/graphql"});
+const httpLink = new HttpLink({ uri: "https://5880-41-203-213-109.ngrok-free.app/graphql"});
 
 
 //Combining Links(link):
