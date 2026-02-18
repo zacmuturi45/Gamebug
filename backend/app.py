1b18777d3edd773813a93a3cfe183c85f5f95204
zacmuturi <zacmuturi45@gmail.com>
2026-02-18 22:48:57 +0300

    Changed app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from src import create_app
 
+app = create_app("production")
+
 if __name__ == "__main__":
-    config_name = "development"
-    app = create_app(config_name)
     app.run(host="0.0.0.0", port=5555)
